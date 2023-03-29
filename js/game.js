2948c7e07585a6e22a23e0db516237064b03af10
Eduarda Isabelle <eduardaisabelle1818@gmail.com>
2023-03-29 06:17:49 -0300

    criando pastas; add imgs

---
+++ js/game.js
@@ -0,0 +1,19 @@
+const grid = document.getElementById('grid');
+const player = document.getElementById('player');
+const timer = document.getElementById('timer');
+const characters = [
+    'benson',
+    'cloudJane',
+    'eillen',
+    'margarete',
+    'mordecai',
+    'morte',
+    'musculoso',
+    'pailurito',
+    'rigbi',
+    'saltitao'
+]
+
+console.log(grid);
+console.log(player);
+console.log(timer);
